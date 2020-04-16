5fb1b5241ad4a5680b0d7577bfee182731222f5a
JiangCan <1101985696@qq.com>
2020-04-16 16:31:26 +0800

    将项目添加到git

---
+++ git-test-project/src/com/jc/helloGit.java
@@ -0,0 +1,12 @@
+package com.jc;
+
+/**
+ * @Descricption:
+ * @Author:江灿
+ * @Date:Create in 16:29 2020/4/16
+ */
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
